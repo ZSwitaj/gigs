3d65e0f943641c2ecd75fa2254e3e22defba7c3a
ZSwitaj <63750319+ZSwitaj@users.noreply.github.com>
2020-10-26 17:50:09 -0400

    Working script with correct topic level searches

--- YOY_searchTrends.py
+++ YOY_searchTrends.py
@@ -1,11 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Oct 21 16:27:18 2020
-
-@author: Administrator
-"""
-
-import matplotlib.pyplot as plt
 import pandas as pd
 from pytrends.request import TrendReq
 from datetime import date
@@ -14,8 +6,8 @@ def getTrendData(keyword):
 
     dataset= []
     
-    end_date = date(2020,9,26)
-    start_date = date(2019, 9, 1)
+    end_date = date(2020,8,29)
+    start_date = date(2019, 7, 28)
     timeFrame = start_date.strftime('%Y-%m-%d')+' ' + end_date.strftime("%Y-%m-%d")
     
     pytrends = TrendReq(hl='en-US', tz=360)
@@ -33,23 +25,70 @@ def getTrendData(keyword):
     lastYear = dataset.head(4)
     thisYear = dataset.tail(4)
     
-    plt.plot(range(4),lastYear, color='blue', label = 'Last Year')
-    plt.plot(range(4), thisYear, color= 'orange', label = 'This Year')
-    plt.legend()
-    
     df = pd.DataFrame()
     df = df.append(lastYear)
     df = df.append(thisYear)
-    
+
     yoyIncrease = ((thisYear.mean(axis=0)[0] - lastYear.mean(axis=0)[0]) / lastYear.mean(axis=0)[0]) * 100
+
+    # print("\n" + keyword + ": " + str(round(yoyIncrease,1)))
+
+    return yoyIncrease
+
+def getSuggestions(keyword):
     
-    # print(pytrends.suggestions(keyword))
+    pytrends = TrendReq(hl='en-US', tz=360)
     
-    print("\n" + keyword + ": " + str(round(yoyIncrease,2)))
+    suggestions = (pytrends.suggestions(keyword))
+    suggestions = pd.DataFrame(suggestions)
+    
+    return suggestions
           
-kw_list = ("Timberland Boots", "UGG", "Dr. Martens", "Merrell", "The North Face",
-           "Patagonia", "ALlbirds", "Vans", "Jimmy Choo")
+# kw_list = ("Zappos", "Foot Locker", "Genesco", "Designer Brands", "Nordstrom", 
+#             "Macy's", "REI", "Dicks", 
+#             "Boot", "Outerwear")
 
+# kw_list = ("Timberland", "Timberland Boots", "UGG", "Dr. Martens", "Merrell", 
+#             "The North Face", "Patagonia", "Allbirds", "Vans", "Amazon", 
+#             "Walmart", "Target")
 
-for i in range(len(kw_list)):
-    getTrendData(kw_list[i])
+# kw_list = ("/m/05kv16","/m/06wccjq")
+
+kw_dict = {
+    "Timberland": "",
+    "UGG": "/m/06wccjq",
+    "Dr Martens": "/m/01lsm6",
+    "Merrell": "/m/0kqrz3",
+    "The North Face": "/m/04n92b",
+    "Patagonia": "/m/0g152j",
+    "Allbirds": "/g/11g6j4k3hl",
+    "Vans": "/m/04kbwy",
+    "Amazon": "/m/0mgkg ",
+    "Walmart": "/m/0841v",
+    "Target": "/m/072p41",
+    }
+
+kw_list = ("Target",)
+
+# for i in range(len(kw_list)):
+#     getTrendData(kw_list[i])
+
+# for topic in kw_dict:
+#     print("{}: {}".format(topic, getTrendData(kw_dict[topic])))
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
